037c1916f2d54ab570117a7e154092ff824e8dfc
Aidan Quayle <aqua109@aucklanduni.ac.nz>
2019-11-21 18:06:13 +1300

    Initial commit

---
+++ Assets/Scripts/PointMover.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System;
+using UnityEngine;
+
+public class PointMover : InputSystemGlobalListener, IMixedRealityInputHandler, IMixedRealityInputHandler<Vector2>, IMixedRealityInputHandler<MixedRealityPose>
+{
+    public MixedRealityInputAction leftTouchpadPress;
+    public MixedRealityInputAction rightTouchpadPress;
+    public MixedRealityInputAction leftTouchpadPosition;
+    public MixedRealityInputAction rightTouchpadPosition;
+    public MixedRealityInputAction leftSpatialPointer;
+    public MixedRealityInputAction rightSpatialPointer;
+    public MixedRealityInputAction leftTrigger;
+    public MixedRealityInputAction rightTrigger;
+
+
+    public float speed = 0.1f;
+
+    private Vector3 localDelta;
+    private Quaternion forwardRotation;
+    public Vector2 lateralRotation;
+    private float rightY;
+
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        if (eventData.MixedRealityInputAction == rightTouchpadPress)
+        {
+            Direction();
+        }
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    {
+        localDelta = new Vector3(0f, 0f, 0f);
+    }
+
+    public void OnInputChanged(InputEventData<Vector2> eventData)
+    {
+        if (eventData.MixedRealityInputAction == rightTouchpadPosition)
+        {
+            rightY = eventData.InputData.y;
+        }
+
+        if (eventData.MixedRealityInputAction == leftTrigger)
+        {
+            //lateralRotation.x = FixDrift(eventData.InputData.y) * -1f;
+            lateralRotation.y = FixDrift(eventData.InputData.x);
+            Debug.Log(lateralRotation.y);
+        }
+    }
+
+    public float FixDrift(float x)
+    {
+        if (x < 0.18f && x > -0.05f)
+        {
+            return 0f;
+        }
+        else
+        {
+            return x;
+        }
+    }
+
+    public void Direction()
+    {
+        Debug.Log(rightY);
+        if (rightY > 0)
+        {
+            Debug.Log("Forward");
+            Vector3 v2tov3 = new Vector3(0f, 0f, 1f);
+            localDelta = speed * v2tov3;
+        }
+        else
+        {
+            Debug.Log("Backward");
+            Vector3 v2tov3 = new Vector3(0f, 0f, -1f);
+            localDelta = speed * v2tov3;
+        }
+    }
+
+    public void OnInputChanged(InputEventData<MixedRealityPose> eventData)
+    {
+        //Debug.Log(eventData.InputData);
+        //Debug.Log(eventData.InputData.Position);
+        //Debug.Log(eventData.InputData.Rotation);
+        if (eventData.MixedRealityInputAction == rightSpatialPointer)
+        {
+            forwardRotation = eventData.InputData.Rotation;
+        }
+    }
+
+    public void Strafe(Vector3 localDelta)
+    {
+        transform.position = transform.position + forwardRotation * localDelta;
+    }
+
+    public void Rotate(Vector2 rotation)
+    {
+        transform.Rotate(rotation.x, rotation.y, 0f);
+    }
+
+    public void Update()
+    {
+        Strafe(localDelta);
+        Rotate(lateralRotation);
+    }
+}
---
+++ Assets/Scripts/ThumbstickMover.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using System;
+using UnityEngine;
+
+public class ThumbstickMover : InputSystemGlobalListener, IMixedRealityInputHandler<Vector2>
+{
+    public MixedRealityInputAction leftTrigger;
+    public MixedRealityInputAction rightTrigger;
+    public float speed = 0.1f;
+
+    public Vector3 localDelta;
+    public Vector2 rotation;
+
+
+    public void OnInputChanged(InputEventData<Vector2> eventData)
+    {
+
+        if (eventData.MixedRealityInputAction == leftTrigger)
+        {
+            // e.g. (0.5, -1) -> (0.5, 0, -1)
+            Vector3 v2tov3 = new Vector3(FixDrift(eventData.InputData.x), 0.0f, FixDrift(eventData.InputData.y));
+            localDelta = speed * v2tov3;
+        }
+
+        if (eventData.MixedRealityInputAction == rightTrigger)
+        {
+            rotation.x = FixDrift(eventData.InputData.y) * -1f;
+            rotation.y = FixDrift(eventData.InputData.x);
+        }
+    }
+
+    public void Strafe(Vector3 localDelta)
+    {
+        transform.position = transform.position + transform.rotation * localDelta;
+    }
+
+    public void Rotate(Vector2 rotation)
+    {
+        transform.Rotate(rotation.x, rotation.y, 0f);
+    }
+
+    public float FixDrift(float x)
+    {
+        if (x < 0.18f && x > -0.05f)
+        {
+            return 0f;
+        }
+        else
+        {
+            return x;
+        }
+    }
+
+    void Update()
+    {
+        Strafe(localDelta);
+        Rotate(rotation);
+    }
+}
